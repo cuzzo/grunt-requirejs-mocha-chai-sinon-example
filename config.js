1e7039c8423c78b8be29ecf545672ad27d1bf16c
Cuzzo Yahn <byahn@IWK010990-088.internal.ngpvan.net>
2013-12-12 03:34:18 -0500

    Remove the need to manually call require.config() in test files and main.

--- config.js
+++ config.js
@@ -1,6 +1,29 @@
-require.config({
-  'paths': {
-    // src
-    'add-one': 'src/add-one'
+(function() {
+  var Config = {
+    'paths': {
+      // src
+      'add-one': 'src/add-one'
+    }
+  };
+
+  if (typeof _TEST_MODE !== 'undefined' && _TEST_MODE === true) {
+    Config.baseUrl = window._BASE_URL;
+    Config.basePath = window._BASE_PATH;
+    require.config(Config);
+    return true;
   }
-});
+
+  if (typeof define === 'function') {
+    define([], function() {
+      return Config;
+    });
+  }
+  if (typeof module === 'object') {
+    module.exports = Config;
+  }
+  if (typeof exports === 'object') {
+    exports.RJSConfig = Config;
+  }
+
+  return Config;
+})();
